1f531a3beff6dba8f00427e9e7c112d605bf32c3
Damien Robert <damien.olivier.robert+git@gmail.com>
2019-04-15 12:49:00 +0200

    config.rb: post_install now install correct (updated) version

--- lib/aur/config.rb
+++ lib/aur/config.rb
@@ -252,15 +252,28 @@ module Archlinux
 			@install_list ||= Archlinux.create_class(@opts[:default_install_list_class], config: self)
 		end
 
-		def post_install(pkgs, **_opts)
+		def post_install(pkgs, **opts)
 			if (db=self.db)
 				tools=local_devtools
 				# info=opts[:pkgs_info]
 				# to_install=info[:all_pkgs].select {|_k,v| v[:op]==:install}.
 				# 	map {|_k,v| v[:out_pkg]}
 				# tools.sync_db(db.repo_name, install: to_install)
+
 				# Let's just install anything and let pacman handle it
-				tools.sync_db(db.repo_name, install: %w(--needed) + pkgs)
+				m=opts[:makepkg_list]
+				if m
+					# we need to update the package versions with the ones
+					# provided by the current makepkg (which may be more recent than
+					# the one from aur's rpc in case of devel packages)
+					ipkgs=pkgs.map do |pkg|
+						found=m.packages.find(Query.strip(pkg))
+						found || pkg
+					end
+				else
+					ipkgs=pkgs
+				end
+				tools.sync_db(db.repo_name, install: %w(--needed) + ipkgs)
 			end
 		end
 	end
